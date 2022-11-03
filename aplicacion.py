8cc0608514a937042d79e184fbd98ed5d0839447
James Huanuco <jamesjustdance2019@gmail.com>
2022-11-03 10:59:12 -0500

    Creado por Huanuco Vicuña, James Nilton

---
+++ aplicacion.py
@@ -0,0 +1,41 @@
+import getpass
+ 
+#declaracion de variables
+
+registeredUser =open("login.txt","rt")
+user = registeredUser.read()
+registeredPW = open("clave.txt","rt")
+password = registeredPW.read() 
+#declaracion de funciones
+def login(usuario,passw):
+    if usuario in user:
+        passw = getpass.getpass('Contraseña: ')
+        if passw in password:
+            return 1
+        else:
+            print("\n\tContraseña Incorrecta, intente de nuevo...\n")
+            passw = getpass.getpass('Contraseña: ')
+            if usuario in user:
+                 if passw in password:
+                     return 1
+                 else:
+                    print("\n\tContraseña Incorrecta\n")
+            else:
+             return 2
+    else:
+        return 2
+ #inicializacion de procesos
+usuario=input('Usuario: ')
+while usuario != user:
+    usuario=input('Usuario: ')
+    passw = "2"
+
+    if login(usuario,passw)==1:
+        print('Bienvenido',usuario)
+        print("\n\tDatos Persona")
+        print("\n\t1.Listar Personas")
+        print("\n\t2.Agregar Personas")
+        print("\n\t3.Salir")
+        
+    else:
+        print('Usuario no registrado')
